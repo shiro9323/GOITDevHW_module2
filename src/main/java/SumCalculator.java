2cff0c5df141e8ceba292974c501cc22cf01b2b8
AndriiSh <trancelvania93@gmail.com>
2023-09-20 21:51:25 +0300

    + test git actions

--- src/main/java/SumCalculator.java
+++ src/main/java/SumCalculator.java
@@ -2,7 +2,8 @@ import java.util.stream.IntStream;
 
 public class SumCalculator {
     public int sum(int n){
-        if (n == 0){
+        if (n == 0)
+        {
             throw new IllegalArgumentException();
         } else return IntStream.rangeClosed(0, n).sum();
     }
